0815d8a7b53c9ec8e47991e238cc871d3e79ae39
Kelechi Otugbuali <hilakechi@gmail.com>
2025-05-21 20:02:02 +0100

    Boiler remplate

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Header } from './component'
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
---
+++ src/component/Header/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Header = () => {
+  return <div className=" px-2 py-4 bg-gray-500"></div>
+}
